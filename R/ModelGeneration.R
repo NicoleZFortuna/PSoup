f8c968322146ebd4322f68c7076696cdf513ff86
Nicole <nicole.fortuna@uqconnect.edu.au>
2022-09-04 22:31:01 +1000

    Applied changes to buildModel function. Deleted test.R.

--- R/ModelGeneration.R
+++ R/ModelGeneration.R
@@ -29,14 +29,15 @@ buildModel <- function(network, upPenalty = NA, containerPenalty = NA, file = ".
   genotypes = network@objects$Genotypes
 
   cat("# defining genotype values\n", file = file)
-  cat("gen = list()\n", file = file, append = T)
+  cat("gen = data.frame(\n", file = file, append = T)
   for (i in 1:length(genotypes)) {
-    cat(paste0("gen$", genotypes[[i]]@name, substr(names(genotypes[[i]]@expression), 1, 1),
-               " = ", genotypes[[i]]@expression), sep = "\n", append = T, file = file)
+    cat(paste0("\tgen$", genotypes[[i]]@name, substr(names(genotypes[[i]]@expression), 1, 1),
+               " = ", genotypes[[i]]@expression), sep = ",\n", append = T, file = file)
   }
+  cat(")\n", file = file, append = T)
 
   cat("\n# defining storage data.frame and node initial values\n", file = file, append = T)
-  cat("dat <- data.frame(\n'", paste(names(nodes), collapse = "' = 1, \n'"), "' = 1\n)\n",
+  cat("dat <- data.frame(\n\t'", paste(names(nodes), collapse = "' = 1, \n\t'"), "' = 1\n)\n",
       sep="", file = file, append = T)
 
   inhibition = c("inhibition", "sufficient inhibition", "necessary inhibition")
@@ -48,42 +49,89 @@ buildModel <- function(network, upPenalty = NA, containerPenalty = NA, file = ".
   cat("\tt = nrow(dat)\n\n", file = file, append = T)
 
   for (i in 1:length(nodes)) {
-    if (any(inhibition %in% nodes[[i]]@inputs$Influence)) {
-      if (any(stimulation %in% nodes[[i]]@inputs$Influence)) {
-        cat(sprintf("\tdat$%s[t] = (2 * %s)/",
-                    nodes[[i]]@name,
-                    paste0("dat$", nodes[[i]]@inputs$Node[nodes[[i]]@inputs$Influence %in% stimulation],
-                           "[t-1]", collapse = " * ")),
-            sprintf("(1 + %s)",
-                    paste0("dat$", nodes[[i]]@inputs$Node[nodes[[i]]@inputs$Influence %in% inhibition],
-                           "[t-1]", collapse = " * ")),
-            if (length(nodes[[i]]@genotypes) == 0) {NULL} else {paste0(" * gen$", nodes[[i]]@genotypes,
-                                                                       substr(nodes[[i]]@container, 1, 1))},
-            "\n", sep = "", append = T,  file = file)
-      } else {
-        cat(sprintf("\tdat$%s[t] = 2/", nodes[[i]]@name),
-            sprintf("(1 + %s)", paste0("dat$", nodes[[i]]@inputs$Node[nodes[[i]]@inputs$Influence %in% inhibition],
-                                       "[t-1]", collapse = " * ")),
-            if (length(nodes[[i]]@genotypes) == 0) {NULL} else {paste0(" * gen$", nodes[[i]]@genotypes,
-                                                                       substr(nodes[[i]]@container, 1, 1))},
-            "\n", sep = "", append = T,  file = file)
+    # collating all the stimulatory action
+    if (any(nodes[[i]]@inputs$Influence == "stimulation")) {
+      stimString <- nodes[[i]]@inputs$Node[nodes[[i]]@inputs$Influence %in% "stimulation" & is.na(nodes[[i]]@inputs$Coregulator)]
+      if (length(stimString) > 0) {stimString <- paste0("dat$", stimString, "[t-1]", collapse = " + ")}
+
+      # are there any stimulants that are coregulators
+      if (any(!is.na(nodes[[i]]@inputs$Coregulator) & nodes[[i]]@inputs$Influence == "stimulation")) {
+        coregInput <- nodes[[i]]@inputs[!is.na(nodes[[i]]@inputs$Coregulator) & nodes[[i]]@inputs$Influence == "stimulation", -3]
+
+        coreg <- coregulators(coregInput)
+
+        stimString = paste(stimString, coreg, sep = " + ")
       }
-    } else {
-      if (any(stimulation %in% nodes[[i]]@inputs$Influence)) {
-        cat(sprintf("\tdat$%s[t] = %s",
-                    nodes[[i]]@name,
-                    paste0("dat$", nodes[[i]]@inputs$Node[nodes[[i]]@inputs$Influence %in% stimulation],
-                           "[t-1]", collapse = " * ")),
-            if (length(nodes[[i]]@genotypes) == 0) {NULL} else {paste0(" * gen$", nodes[[i]]@genotypes,
-                                                                       substr(nodes[[i]]@container, 1, 1))},
-            "\n", sep = "", append = T,  file = file)
-      } else {
-        cat(sprintf("\tdat$%s[t] = 1", nodes[[i]]@name),
-            if (length(nodes[[i]]@genotypes) == 0) {NULL} else {paste0(" * gen$", nodes[[i]]@genotypes,
-                                                                       substr(nodes[[i]]@container, 1, 1))},
-            "\n", sep = "", append = T,  file = file)
+    } else {stimString = NA}
+
+    # collating all the inhibitory action
+    if (any(nodes[[i]]@inputs$Influence == "inhibition")) {
+      inhibString <- nodes[[i]]@inputs$Node[nodes[[i]]@inputs$Influence %in% "inhibition" & is.na(nodes[[i]]@inputs$Coregulator)]
+      numInhib <- length(inhibString)
+      if (length(inhibString) > 0) {inhibString <- paste0("dat$", inhibString, "[t-1]", collapse = " + ")}
+
+      # are there any inhibitors that are coregulators
+      if (any(!is.na(nodes[[i]]@inputs$Coregulator) & nodes[[i]]@inputs$Influence == "inhibition")) {
+        coregInput <- nodes[[i]]@inputs[!is.na(nodes[[i]]@inputs$Coregulator) & nodes[[i]]@inputs$Influence == "inhibition", -3]
+
+        coreg <- coregulators(coregInput)
+
+        numInhib = coreg$num
+        inhibString = paste(inhibString, coreg$coreg, sep = " + ")
       }
+    } else {inhibString = NA}
+
+    # combining stimulatory and inhibitory effects
+    if (class(stimString) == "character" & is.na(inhibString)) {
+      # if there are only stimulatory effects
+      allModulations <- stimString
+    } else if (class(inhibString) == "character" & is.na(stimString)) {
+      # if there are only inhibitory effects
+      allModulations <- sprintf("%s/(1 + %s)", numInhib + 1, inhibString)
+    } else if (class(stimString) == "character" & class(inhibString) == "character") {
+      allModulations <- sprintf("(%s*%s)/(1 + %s)", numInhib + 1, stimString, inhibString)
+    } else if (is.na(stimString) & is.na(inhibString)) {
+      # if it is constituent wo influence from other nodes
+      allModulations <- 1
     }
+
+    # multiplying modulations by necessary stimulants and genotypes
+    if (any(nodes[[i]]@inputs$Influence == "necessary stimulation")) {
+      necstimString <- nodes[[i]]@inputs$Node[nodes[[i]]@inputs$Influence %in% "necessary stimulation" & is.na(nodes[[i]]@inputs$Coregulator)]
+      if (length(necstimString) > 0) {necstimString <- paste0("dat$", necstimString, "[t-1]", collapse = " + ")}
+
+      # are there any necessary stimulants that are coregulators
+      if (any(!is.na(nodes[[i]]@inputs$Coregulator) & nodes[[i]]@inputs$Influence == "stimulation")) {
+        coregInput <- nodes[[i]]@inputs[!is.na(nodes[[i]]@inputs$Coregulator) & nodes[[i]]@inputs$Influence == "necessary stimulation", -3]
+
+        coreg <- coregulators(coregInput)
+
+        necstimString = paste(necstimString, coreg, sep = " + ")
+      }
+      allModulations <- sprintf("(%s) * (%s)", allModulations, necstimString)
+    }
+
+    if (length(nodes[[i]]@genotypes) > 0) {
+      genes <- nodes[[i]]@genotypes
+      genoString <- rep(NA, length(genes))
+      for (g in 1:length(genes)) {
+        if (class(genotypes[[genes[g]]]@coregulator) == "character") {
+          cogenes <- c(genes[g], genotypes[[genes[g]]]@coregulator)
+
+          genoString[g] <- paste0(cogenes[order(cogenes)], collapse = "*")
+        }
+      }
+
+      genoString = unique(genoString)
+      allModulations <- sprintf("(%s) * (%s)", allModulations, genoString)
+    }
+
+    # add if there is a source from another node (will not be influenced by dynamics of current node)
+    if (any(nodes[[i]]@inputs$Influence == "altSource")) {
+      altString <- nodes[[i]]@inputs$Node[nodes[[i]]@inputs$Influence %in% "altSource"]
+      allModulations <- sprintf("%s + %s", paste0("dat$", altString, "[t-1]", collapse = " + "), allModulations)
+    }
+    cat(paste0("\tdat$",nodes[[i]]@name, "[t-1] = ", allModulations, "\n"), file = file, append = T)
   }
   cat("\tdat[t, ]\n", file = file, append = T)
   cat("}", file = file, append = T)
