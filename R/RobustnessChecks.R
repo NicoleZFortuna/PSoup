9759693c38f1efb3249490735fc8a757c13db021
Nicole Fortuna <n.fortuna@uq.edu.au>
2023-08-24 17:05:11 +1000

    began checkEdgeTypes function

--- R/RobustnessChecks.R
+++ R/RobustnessChecks.R
@@ -2,8 +2,21 @@
 #' nature of edge types.
 #'
 #' @param startingNetwork an object of class network describing
+#' @param nodeStartVals description
+#' @param genotypeVals description
 
-checkEdgeTypes <- function(startingNetwork) {
+checkEdgeTypes <- function(startingNetwork, nodeStartVals, genotypeVals) {
+  # finding the dimensionality of the data to be generated
+  modelNo <- nrow(nodeStartVals) * nrow(genotypeVals)
+  networkNo <- sum(sapply(peaNetwork@objects$Hormones, function(x) nrow(x@inputs)))
 
+  # stating the conditions to be explored
+  modelConditions <-
+
+
+  dat <- array(NA, dim = c(modelNo, length(peaNetwork@objects$Hormones), networkNo),
+               dimnames = list("ModelConditions", peaNetwork@objects$Hormones, "NetworkConfiguration"))
 }
 
+
+sapply(peaNetwork@objects$Hormones, function(x) nrow(x@inputs))
