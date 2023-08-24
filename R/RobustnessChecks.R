beeb8f3a9134f352a4dbaa23ec6be10cb12de055
Nicole Fortuna <n.fortuna@uq.edu.au>
2023-08-24 10:48:26 +1000

    Added R/RobustnessChecks.R which will house functions relating to testing how robust the behaviour of a network is.

---
+++ R/RobustnessChecks.R
@@ -0,0 +1,9 @@
+#' A function to test how network behavior changes in response to changing the
+#' nature of edge types.
+#'
+#' @param startingNetwork an object of class network describing
+
+checkEdgeTypes <- function(startingNetwork) {
+
+}
+
