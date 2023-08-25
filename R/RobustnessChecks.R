896014429bb8d4cb4dbd9598272fd58727fe18ff
Nicole Fortuna <n.fortuna@uq.edu.au>
2023-08-25 16:47:06 +1000

    checkEdgeTypes function

--- R/RobustnessChecks.R
+++ R/RobustnessChecks.R
@@ -7,15 +7,41 @@
 
 checkEdgeTypes <- function(startingNetwork, nodeStartVals, genotypeVals) {
   # finding the dimensionality of the data to be generated
-  modelNo <- nrow(nodeStartVals) * nrow(genotypeVals)
-  networkNo <- sum(sapply(peaNetwork@objects$Hormones, function(x) nrow(x@inputs)))
+
+  nonWT <- function(x) {
+    nWT <- which(x != 1)
+    dat <- data.frame(node = attributes(x)$names[nWT], val = as.numeric(x)[nWT])
+    model <- paste(apply(dat, 1, paste0, collapse = ""), collapse = "_")
+    if (model == "") return("WT")
+    else return(model)
+  }
 
   # stating the conditions to be explored
-  modelConditions <-
+  #nodeModelConditions <- apply(nodeStartVals, 1, nonWT)
+  geneModelConditions <- apply(genotypeVals, 1, nonWT)
+
+  networkChanges <- function(x) {
+    edges <- c("stimulation", "inhibition")
+    toChange <- which(x@inputs$Influence %in% edges)
+
+    if (nrow(x@inputs) == 0) {
+      return(NULL)
+    } else {
+      newEdge <- sapply(-match(x@inputs$Influence[toChange], edges), function(y) edges[y])
+
+      return(paste0(x@inputs$Node, "->", x@name, ".", newEdge))
+    }
+  }
+
+  networkAlternations <- unlist(lapply(peaNetwork@objects$Hormones, networkChanges), use.names = F)
 
 
-  dat <- array(NA, dim = c(modelNo, length(peaNetwork@objects$Hormones), networkNo),
-               dimnames = list("ModelConditions", peaNetwork@objects$Hormones, "NetworkConfiguration"))
+  dat <- array(NA, dim = c(length(geneModelConditions),
+                           length(peaNetwork@objects$Hormones),
+                           length(networkAlternations) + 1),
+               dimnames = list(geneModelConditions,
+                               names(peaNetwork@objects$Hormones),
+                               c("startingNetwork", networkAlternations)))
 }
 
 
